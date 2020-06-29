4c92e5d6176172930bf8e9a5a83d7a8ed2793756
Waqar Ahmed <57212390+WaqarAhmedk@users.noreply.github.com>
2020-06-29 19:56:00 +0500

    added devolper name +css

--- src/child.js
+++ src/child.js
@@ -1,55 +1,58 @@
 import React, { useContext, useState } from 'react';
-import { TransactionContext} from './TransactionContext';
+import { TransactionContext } from './TransactionContext';
 function Child() {
-    let  {transactions,addTransaction} = useContext(TransactionContext);
-    let [newDesc , setDesc]=useState('');
-    let [newAmount , setAmount]=useState(0.0);
-    
+    let { transactions, addTransaction } = useContext(TransactionContext);
+    let [newDesc, setDesc] = useState('');
+    let [newAmount, setAmount] = useState(0.0);
 
-    
-    const handleAddition=(event)=>{
+
+
+    const handleAddition = (event) => {
 
 
         event.preventDefault();
-        if (Number(newAmount==0)){
+        if (Number(newAmount == 0)) {
             alert("please enter correct amount");
             return false;
         }
 
-        console.log(newDesc,newAmount)
+        console.log(newDesc, newAmount)
         addTransaction({
             Amount: Number(newAmount),
-        
-            Desc:newDesc
+
+            Desc: newDesc
         });
         setDesc('');
-        setAmount(0.0);   
+        setAmount(0.0);
     }
-    const getIncome=()=>{
-        let income=0.0;
-        for(var a=0; a<transactions.length;a++){
-            if(transactions[a].Amount>0){
-            income=income+transactions[a].Amount
+    const getIncome = () => {
+        let income = 0.0;
+        for (var a = 0; a < transactions.length; a++) {
+            if (transactions[a].Amount > 0) {
+                income = income + transactions[a].Amount
             }
         }
         return income;
     }
-    const getExpense=()=>{
-        let expense=0.0;
-        for(var a=0; a<transactions.length;a++){
-            if(transactions[a].Amount<0)
-            expense=expense+transactions[a].Amount
+    const getExpense = () => {
+        let expense = 0.0;
+        for (var a = 0; a < transactions.length; a++) {
+            if (transactions[a].Amount < 0)
+                expense = expense + transactions[a].Amount
 
         }
         return expense;
     }
     return (
         <div className='container'>
-            <h1 className='head'>Expense Tracker<br/></h1>
-    <h2 className='bal'>Your Balance <br /> ${getIncome()+getExpense()}</h2>
+            <div className='head'>
+                <h1 >Expense Tracker<sub className='sub'> by WAQAR AHMED</sub></h1>
+            </div>
+
+            <h2 className='bal'>Your Balance <br /> ${getIncome() + getExpense()}</h2>
             <div className='incexp'>
-    <h2>Income <br />${getIncome()}</h2>
-    <h2>Expense <br />${getExpense()}</h2>
+                <h2>Income <br />${getIncome()}</h2>
+                <h2>Expense <br />${getExpense()}</h2>
             </div>
             <h2 className='bal'>History</h2>
             <hr />
@@ -70,17 +73,17 @@ function Child() {
                     <label className='bal'>
                         Enter Description <br />
                         <input type='text'
-                         onChange={(ev)=>setDesc (ev.target.value)}
-                          required  />
+                            onChange={(ev) => setDesc(ev.target.value)}
+                            required />
                         <br />
                     </label>
                     <label className='bal'>
                         Enter Amount <br />
-                        <input type='number'onChange={(ev)=>setAmount(ev.target.value)} required />
+                        <input type='number' onChange={(ev) => setAmount(ev.target.value)} required />
                         <br />
                     </label>
-                    <br/>
-                    <input type='submit'  className='bal' value='Add transction' />
+                    <br />
+                    <input type='submit' className='bal' value='Add transction' />
                 </form>
             </div>
         </div>
